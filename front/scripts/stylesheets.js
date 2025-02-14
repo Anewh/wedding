834d81df054f0197f2b6b687697aace4f24a3079
Anewh <i0ianrusi0i@gmail.com>
2025-02-15 02:58:22 +0300

    страница видна после прогрузки всех элементов

--- front/scripts/stylesheets.js
+++ front/scripts/stylesheets.js
@@ -3,47 +3,7 @@ import '../stylesheets/main.css';
 
 import $ from 'jquery'
 $(document).ready(function(){
-  
-    const prevBtn = document.querySelector('#prevBtn');
-    const nextBtn = document.querySelector('#nextBtn');
-    const scrollContainer = document.querySelector('.list');
-    const testOutput = document.querySelector('#output');
-    
-    prevBtn.onclick = () => {
-      scrollContainer.scrollBy({
-        top: 0,
-        left: -100,
-        behavior: "smooth",
-        selectedAttraction: 0.01,
-        friction: 0.15
-      }) 
-    };
-    
-    nextBtn.onclick = () => {
-      scrollContainer.scrollBy({
-        top: 0,
-        left: +100,
-        behavior: "smooth",
-        selectedAttraction: 0.01,
-        friction: 0.15
-      }) 
-    };
-    
-    scrollContainer.addEventListener("scrollend", () => {
-      if(scrollContainer.scrollLeft === 0) {
-        prevBtn.disabled = true;
-        return;
-      }
-      
-      const maxScrollLeft = scrollContainer.scrollWidth - scrollContainer.clientWidth
-      
-      if(scrollContainer.scrollLeft === maxScrollLeft) {
-        nextBtn.disabled = true;
-        return;
-      }
-      prevBtn.disabled = false;
-      nextBtn.disabled = false;
-    });
+  document.getElementsByTagName("html")[0].style.visibility = "visible";
   });
 
 
