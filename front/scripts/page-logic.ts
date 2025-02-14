0b00aa94834447e0a11ce768a4e3ea01235470da
Terqaz <holinvova@gmail.com>
2025-02-14 23:48:23 +0300

    анимации для конверта

--- front/scripts/page-logic.ts
+++ front/scripts/page-logic.ts
@@ -17,3 +17,33 @@ export function changeAfterSendEatPreferences() {
         choosePreferencesWrapper.style.display = 'none';
     }
 }
+
+document.addEventListener('DOMContentLoaded', e => {
+    setInterval(goToMainPage, 500);
+});
+document.getElementById('envelope-check')?.addEventListener('change', goToMainPage);
+
+function goToMainPage(e) {
+    const envelopeCheck = document.getElementById('envelope-check') as HTMLInputElement | null;
+    const welcomePage = document.getElementById('welcome-page');
+    const mainPage = document.getElementById('main-page');
+
+    if (!envelopeCheck || !welcomePage || !mainPage) {
+        return;
+    }
+
+    const exitWelcomePage = () => {
+        welcomePage.style.display = 'none';
+    };
+
+    const fadeOutWelcomePage = () => {
+        welcomePage?.classList.add('fade-out');
+
+        setInterval(exitWelcomePage, 500);
+    };
+
+    if (envelopeCheck.checked) {
+        mainPage.style.display = 'flex';
+        setInterval(fadeOutWelcomePage, 500);
+    }
+}
